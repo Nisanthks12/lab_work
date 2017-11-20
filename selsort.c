e94b757646306b6548befab20d094f67c1a72681
Nisanthks12 <nisanthks12@gmail.com>
2017-11-20 20:25:38 +0530

    Update selsort.c

--- selsort.c
+++ selsort.c
@@ -1,28 +1,29 @@
+//Program to perform array sorting using Selection Sort Algorithm
 #include<stdio.h>
+
 main()
-{
- int a[10],n,i,j,temp,min;
- printf("enter the limit");
- scanf("%d",&n);
- printf("enter the elements ");
+{ int a[10],n,i,j,temp,min;
+  printf("Enter the limit");
+  scanf("%d",&n);                       //Inputing the array size limit
+ 
+  printf("Enter the elements ");
   for(i=0;i<n;i++)
-   scanf("%d",&a[i]);
-  for(i=0;i<=n-1;i++)
- { min=i;
-  for(j=i+1;j<=n-1;j++)
-  { if(a[j]<a[min])
-    { min=j;
-    } 
+   scanf("%d",&a[i]);                   //Reading the elements to the array
+  
+ for(i=0;i<=n-1;i++)                   //Loop to iterate through all the elements  
+  { min=i;                             //To initialize the position of minimum element
+    for(j=i+1;j<=n-1;j++)              //To compare the 'min' element with rest of the element to its right
+    { if(a[j]<a[min])                  //Checking if there is a smaller element 
+       min=j;                          //If yes, assign the position of that element to 'min'
+    }
+    if(min!=i)                         //Checking if there is any change in 'min'
+    { temp=a[min];                     //If yes, swap the element in position 'min' and 'i' 
+      a[min]=a[i];
+      a[i]=temp;
+    }
   }
-  if(min!=i)
-  {
-   temp=a[min];
-   a[min]=a[i];
-   a[i]=temp;
-  }
- }
- printf(" the sorted array is ");
+ 
+ printf("The sorted array is ");       //Print the sorted array
   for(i=0;i<n;i++)
-  { printf("%d ",a[i]);
-  }
+   printf("%d ",a[i]);
 }
